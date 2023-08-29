c2b88532f309570a6be44d1420e231cb6825d813
jay-o-sullivan <>
2023-08-29 14:02:49 +0000

    add user input to menu , select component, introduction, addpublish build, esarch by name. update comments

--- run.py
+++ run.py
@@ -15,25 +15,87 @@ def type_effect(text):
 
 # Function to display a menu and get user input
 def display_menu(options):
-    for index, option in enumerate(options, start=1):
-        print(f"{index}. {option}")
-    choice = input("Select an option: ")
-    return choice
+    while True:
+        for index, option in enumerate(options, start=1):
+            print(f"{index}. {option}")
+        try:
+            choice = int(input("Select an option: "))
+            if 1 <= choice <= len(options):
+
+                return str(choice)
+            else:
+                print("Invalid input. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+# Function to get valid integer input from the user
+def get_valid_integer_input(prompt):
+    while True:
+        try:
+            choice = int(input(prompt))
+            return choice
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 
 # Function to select a component from a given sheet
 def select_component(sheet, component_type):
+    # Display available components
     print(f"Available {component_type}s:")
     components = sheet.get_all_records()
     for index, component in enumerate(components, start=1):
         print(f"{index}. {component['Model']} - ${component['Price']}")
-    choice = int(input(f"Select a {component_type} (enter number): ")) - 1
-    selected_component = components[choice]
-    return selected_component
+
+    while True:
+        try:
+            # Get user input
+            choice = input(f"Select a {component_type} (enter number): ")
+
+            # Check if user input is not empty
+            if choice:
+                choice = int(choice) - 1
+
+                # Check if the choice is within valid range
+                if 0 <= choice < len(components):
+                    selected_component = components[choice]
+                    return selected_component
+                else:
+                    print("Invalid choice. Please select a valid number.")
+            else:
+                print("Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+# Function to display an introduction to the app
+def display_introduction():
+    type_effect("Welcome to PC Part Picker!")
+    type_effect
+    ("This application allows you to build your own PC configuration \
+      or choose from prebuilt options.")
+    type_effect("You can also save your custom "
+                "builds and search for them later.")
+    type_effect("Let's get started!\n")
+
+
+def search_build_by_name(sheet, name):
+    builds = sheet.get_all_records()
+    matching_builds = []
+
+    print(f"Searching for builds with name '{name}'")
+
+    for build in builds:
+        if 'Name' in build and 'Configuration' in build and 'Total Price' in build:
+            if name.lower() in build['Name'].lower().strip() or name.lower() in build['Configuration'].lower().strip():
+                matching_builds.append(build)
+                print(f"Match found: {build['Name']} - {build['Configuration']}")
+
+    return matching_builds
 
 
 def main():
-    type_effect("Welcome to PC Part Picker!\n")
+    display_introduction()
 
     SCOPE = [
         "https://www.googleapis.com/auth/spreadsheets",
@@ -57,11 +119,17 @@ def main():
     powersupply_sheet = SHEET.worksheet("PowerSupply")
     case_sheet = SHEET.worksheet("Case")
 
+    user_name = input("Enter your name: ")
+
     while True:
         type_effect("Choose an option:\n")
-        option = display_menu(["Build a PC", "Prebuilt PCs", "Exit"])
+        option = display_menu(
+            ["Build a PC", "Prebuilt PCs", "Search Builds by Name", "Exit"]
+        )
         os.system('clear' if os.name == 'posix' else 'cls')
         print()
+
+        # Build a custom PC configuration
         if option == "1":
             cpu = select_component(cpu_sheet, "CPU")
             print()
@@ -78,39 +146,61 @@ def main():
             case = select_component(case_sheet, "Case")
             print()
 
-            # Calculate total price of the build
-            total_price = cpu["Price"] + motherboard["Price"] + gpu["Price"] + ram["Price"] + storage["Price"] + powersupply["Price"] + case["Price"]
+            total_price = (
+                cpu["Price"] + motherboard["Price"] +
+                gpu["Price"] + ram["Price"] + storage["Price"] +
+                powersupply["Price"] + case["Price"]
+            )
+
             print(f"Total Price: {total_price}\n")
 
-            builds_sheet.append_row([
-                cpu["Model"],
-                motherboard["Model"],
-                gpu["Model"],
-                ram["Model"],
-                storage["Model"],
-                powersupply["Model"],
-                case["Model"],
-                total_price
-            ])
-            print("Build configuration saved to 'Builds' worksheet.\n")
+            publish_build = input(
+                "Do you want to publish this build? (yes/no): "
+                )
+            if publish_build.lower() == "yes":
+                # Get the build name from the user
+                build_name = input("Enter a name for this build: ")
+                # Save as custom build
+                builds_sheet.append_row([
+                    user_name,
+                    build_name,
+                    cpu["Model"],
+                    motherboard["Model"],
+                    gpu["Model"],
+                    ram["Model"],
+                    storage["Model"],
+                    powersupply["Model"],
+                    case["Model"],
+                    total_price
+                ])
+                print(f"Build configuration '{build_name}' saved to 'Builds' worksheet.\n")
 
         elif option == "2":
-            prebuilt_option = display_menu(["Under $1000", "Under $2000"])
+            while True:
+                print("Viewing prebuilt configurations:")
+                prebuilt_option = input("Select prebuilt option (1/2): ")
+                if prebuilt_option in ["1", "2"]:
+                    break
+                else:
+                    print("Invalid input. Please enter either '1' or '2'.")
+            prebuilt_option = int(prebuilt_option)  # Convert to int
+
             prebuilt_records = prebuilts_sheet.get_all_records()
-            #print(prebuilt_records)
             matching_prebuilts = []
 
             for prebuilt in prebuilt_records:
                 configuration = prebuilt["Configuration"]
-                if prebuilt_option in configuration:
+                if prebuilt_option == 1 and "Under $1000" in configuration:
+                    matching_prebuilts.append(prebuilt)
+                elif prebuilt_option == 2 and "Under $2000" in configuration:
                     matching_prebuilts.append(prebuilt)
 
             if matching_prebuilts:
-                print(f"Matching {prebuilt_option} prebuilt configurations:")
+                print(f"Matching {'Under $1000' if prebuilt_option == 1 else 'Under $2000'} prebuilt configurations:")
                 for idx, prebuilt in enumerate(matching_prebuilts, start=1):
                     print(f"{idx}. Configuration: {prebuilt['Configuration']}")
 
-                selected_idx = int(input("Select a prebuilt configuration (enter number): ")) - 1
+                selected_idx = get_valid_integer_input("Select a prebuilt configuration (enter number): ") - 1
 
                 if 0 <= selected_idx < len(matching_prebuilts):
                     selected_prebuilt = matching_prebuilts[selected_idx]
@@ -122,13 +212,52 @@ def main():
                     print(f"Storage: {selected_prebuilt['Storage Model']}")
                     print(f"PowerSupply: {selected_prebuilt['PowerSupply Model']}")
                     print(f"Case: {selected_prebuilt['Case Model']}")
-                    print(f"Total Price: {selected_prebuilt['Total Price']}")
+                    print(f"Total Price: {selected_prebuilt['Total Price']}\n")
+
+                    input(
+                        "Press Enter to return to the PC configuration selection screen..."
+                        )
+            else:
+                print(f"No matching {'Under $1000' if prebuilt_option == 1 else 'Under $2000'} prebuilt configurations found.\n")
 
         elif option == "3":
-                    type_effect("Thank you for using PC Part Picker!\n")
-                    break
+            # Search for builds by name
+            search_name = input("Enter the name to search for builds: ")
+            matching_builds = search_build_by_name(builds_sheet, search_name)
+
+            if matching_builds:
+                # Display only the matching build information
+                matching_build = matching_builds[0]
+                print("Matching build:")
+                print(f"Name: {matching_build['Name']}")
+                print(f"Configuration: {matching_build['Configuration']}")
+                print(f"Total Price: ${matching_build['Total Price']:.2f}\n")
+
+                # Display the components for the matching build
+                components = [
+                    "CPU",
+                    "Motherboard",
+                    "GPU",
+                    "RAM",
+                    "Storage",
+                    "Power Supply",
+                    "Case"]
+                print("\nComponents:")
+                for component in components:
+                    if component in matching_build:
+                        print(f"{component}: {matching_build[component]}")
+
+                print()  # Print an empty line for spacing
+            else:
+                print("No matching builds found.\n")
+
+        elif option == "4":
+            type_effect("Thank you for using PC Part Picker!\n")
+            break
 
         else:
-                    print("Invalid selection.\n")
+            print("Invalid selection.\n")
+
+
 if __name__ == "__main__":
     main()
